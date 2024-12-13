e6610ba9801f0524bce3e133135123f82d209b0b
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2024-12-13 11:15:11 -0600

    Getting back on writing

--- 2 Output/Ex 2B/MSU/process raw output.R
+++ 2 Output/Ex 2B/MSU/process raw output.R
@@ -115,8 +115,8 @@ JOL$Response.Response = tolower(JOL$Response.Response)
 Study$Response.Response = tolower(Study$Response.Response)
 
 #now write to .csv for scoring
-length(unique(JOL$Username)) #40
-length(unique(Study$Username)) #39
+length(unique(JOL$Username)) #38
+length(unique(Study$Username)) #38
 
 #add source
 JOL$source = rep("MSU")
--- 2 Output/Ex 2B/USM/process raw output.R
+++ 2 Output/Ex 2B/USM/process raw output.R
@@ -115,8 +115,8 @@ JOL$Response.Response = tolower(JOL$Response.Response)
 Study$Response.Response = tolower(Study$Response.Response)
 
 #now write to .csv for scoring
-length(unique(JOL$Username)) #25
-length(unique(Study$Username)) #26
+length(unique(JOL$Username)) #23
+length(unique(Study$Username)) #25
 
 #add source
 JOL$source = rep("USM")
