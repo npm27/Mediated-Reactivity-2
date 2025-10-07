994bd9f34222aac2bfab07e3f5fec4931869984d
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2025-10-07 16:26:12 -0500

    Done for now

--- 3 Analyses/Cross Experimental/Cross Experiment.R
+++ 3 Analyses/Cross Experimental/Cross Experiment.R
@@ -64,3 +64,82 @@ tapply(forward_M_R$score, list(forward_M_R$experiment, forward_M_R$direction), m
 ###Pos-hocs (break down the two-way interactions)
 
 ####Now for the "B" experiments####
+model2 = ezANOVA(backward_M,
+                 wid = id,
+                 between = .(encoding, experiment),
+                 within = direction,
+                 dv = score,
+                 type = 3,
+                 detailed = T)
+
+model2$ANOVA$MSE = model2$ANOVA$SSd/model2$ANOVA$DFd
+model2$ANOVA$MSE
+
+aovEffectSize(model2, effectSize = "pes")
+
+backward_M_JOL = subset(backward_M, backward_M$encoding == "JOL")
+backward_M_R = subset(backward_M, backward_M$encoding == "Read")
+
+####T test comparisions for GD####
+##Forward Experiments
+forward_MR.wide.1 = cast(subset(forward_M_R,
+                                forward_M_R$experiment == "Ex1A"), id ~ direction, value = "score", mean)
+forward_MR.wide.2 = cast(subset(forward_M_R,
+                                forward_M_R$experiment == "Ex2A"), id ~ direction, value = "score", mean)
+
+##backward experiments
+backward_MR.wide.1 = cast(subset(backward_M_R,
+                                backward_M_R$experiment == "Ex1B"), id ~ direction, value = "score", mean)
+backward_MR.wide.2 = cast(subset(backward_M_R,
+                                backward_M_R$experiment == "Ex2B"), id ~ direction, value = "score", mean)
+
+##run the t.tests
+#As
+temp = t.test(forward_MR.wide.1$M, forward_MR.wide.2$M, paired = F, p.adjust.methods = "bonferroni", var.equal = T)
+temp
+round(temp$p.value, 3)
+temp$statistic #sig!
+(temp$conf.int[2] - temp$conf.int[1]) / 3.92
+
+mean(forward_MR.wide.1$M); mean(forward_MR.wide.2$M)
+sd(forward_MR.wide.1$M); sd(forward_MR.wide.2$M)
+
+pbic1 = forward_MR.wide.1[ c(1, 3)]
+pbic2 = forward_MR.wide.2[ c(1, 3)]
+
+pbic1$ex = rep(1)
+pbic2$ex = rep(2)
+
+pbic3 = rbind(pbic1, pbic2)
+
+ezANOVA(pbic3,
+        wid = id,
+        between = ex,
+        dv = M,
+        type = 3,
+        detailed = T)
+
+#Bs
+temp = t.test(backward_MR.wide.1$M, backward_MR.wide.2$M, paired = F, p.adjust.methods = "bonferroni", var.equal = T)
+temp
+round(temp$p.value, 3)
+temp$statistic 
+(temp$conf.int[2] - temp$conf.int[1]) / 3.92
+
+mean(backward_MR.wide.1$M); mean(backward_MR.wide.2$M)
+sd(backward_MR.wide.1$M); sd(backward_MR.wide.2$M)
+
+pbic1 = backward_MR.wide.1[ c(1, 3)]
+pbic2 = backward_MR.wide.2[ c(1, 3)]
+
+pbic1$ex = rep(1)
+pbic2$ex = rep(2)
+
+pbic3 = rbind(pbic1, pbic2)
+
+ezANOVA(pbic3,
+        wid = id,
+        between = ex,
+        dv = M,
+        type = 3,
+        detailed = T)
